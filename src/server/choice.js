6711661cf199a66c8e46e7f75ad2d6b9276fe0a9
Gnxela <qawswedfrgtt@gmail.com>
2019-06-18 13:37:59 +0000

    Added choice class

---
+++ src/server/choice.js
@@ -0,0 +1,10 @@
+function Choice(id, user, text, options, deadline) {
+	this.id = id;
+	this.user = user;
+	this.choiceId = choiceId;
+	this.text = text;
+	this.options = options;
+	this.deadline = deadline;
+}
+
+exports = module.exports = Choice;
